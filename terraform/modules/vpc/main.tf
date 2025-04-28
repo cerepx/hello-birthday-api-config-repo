f6c6f2902ee2d446b71761b6a2a4b7ed53591b90
Constantin Cirimpei <constantin.cirimpei@ppb.com>
2025-04-28 11:06:26 +0300

    fix: add IAM role and CloudWatch log group for VPC flow logs

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -147,7 +147,7 @@ resource "aws_flow_log" "vpc_flow_logs" {
   vpc_id               = aws_vpc.this.id
   log_destination      = aws_cloudwatch_log_group.vpc_flow_logs.arn
   log_destination_type = "cloud-watch-logs"
-  iam_role_arn              = aws_iam_role.flow_logs_role.arn
+  iam_role_arn         = aws_iam_role.flow_logs_role.arn
   traffic_type         = "ALL"
 }
 
